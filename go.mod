6f127b4adf5feb6e99b4938921a76d15a2849b96
Stefan Arentz <stefan@arentz.ca>
2025-06-04 10:12:20 -0400

    Implement ipify client for retrieving public IP (#1)

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/epsilonrhorho/dns-updater
 
-go 1.24.3
+go 1.23
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"context"
+	"log"
+
+	"github.com/epsilonrhorho/dns-updater/ipify"
+)
+
 func main() {
-	print("Hello")
+	client := ipify.NewClient(nil)
+	ip, err := client.GetIP(context.Background())
+	if err != nil {
+		log.Fatalf("failed to get IP: %v", err)
+	}
+	log.Println("Public IP:", ip)
 }
