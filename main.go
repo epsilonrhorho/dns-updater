146afbd7b14703f9c26998f475a2ded71667cf1b
Stefan Arentz <stefan@arentz.ca>
2025-06-04 10:38:28 -0400

    Document env vars and IAM policy

--- main.go
+++ main.go
@@ -3,8 +3,14 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 
 	"github.com/epsilonrhorho/dns-updater/ipify"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/route53"
+	"github.com/aws/aws-sdk-go-v2/service/route53/types"
 )
 
 func main() {
@@ -14,4 +20,38 @@ func main() {
 		log.Fatalf("failed to get IP: %v", err)
 	}
 	log.Println("Public IP:", ip)
+
+	zoneID := os.Getenv("HOSTED_ZONE_ID")
+	if zoneID == "" {
+		log.Fatal("HOSTED_ZONE_ID environment variable is required")
+	}
+
+	cfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		log.Fatalf("failed to load AWS config: %v", err)
+	}
+
+	r53 := route53.NewFromConfig(cfg)
+
+	_, err = r53.ChangeResourceRecordSets(context.Background(), &route53.ChangeResourceRecordSetsInput{
+		HostedZoneId: aws.String(zoneID),
+		ChangeBatch: &types.ChangeBatch{
+			Changes: []types.Change{
+				{
+					Action: types.ChangeActionUpsert,
+					ResourceRecordSet: &types.ResourceRecordSet{
+						Name:            aws.String("raspberrypi.epsilonrhorho.club"),
+						Type:            types.RRTypeA,
+						TTL:             aws.Int64(300),
+						ResourceRecords: []types.ResourceRecord{{Value: aws.String(ip)}},
+					},
+				},
+			},
+		},
+	})
+	if err != nil {
+		log.Fatalf("failed to update Route53 record: %v", err)
+	}
+
+	log.Println("Route53 A record updated")
 }
